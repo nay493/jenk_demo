7986deae47ef215d2d61abae657f0a05c85c595a
Mohammed Nayeem <mohammednayeem45@gmail.com>
2020-01-09 21:01:10 +0530

    add subtraction

--- adder.c
+++ adder.c
@@ -2,10 +2,12 @@
 
 int main() {
 
-	int a,b,c;
+	int a,b,c,d;
 	a=2;
 	b=4;
 	c = a + b;
+	d = a - b;
 	printf("addition of %d & %d is %d\n",a,b,c);
+	printf("subtraction of %d & %d is %d\n",a,b,d);
 	return 0;
 }
